47f8b1d7b888aca80a3e1a143dd106664c5440b7
MPawezka <marcin.pawezka@gmail.com>
2023-01-05 09:32:16 +0100

    day 3 solution

--- day003/src/test/java/com/pawelpluta/day003/ParameterizedTest.java
+++ day003/src/test/java/com/pawelpluta/day003/ParameterizedTest.java
@@ -1,8 +1,10 @@
 package com.pawelpluta.day003;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static java.math.BigDecimal.ONE;
 import static java.math.BigDecimal.valueOf;
@@ -11,47 +13,36 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ParameterizedTest {
 
-    private DummyCalculator calculator = new DummyCalculator();
+    private final DummyCalculator calculator = new DummyCalculator();
 
-    @Test
-    void twoPositiveNumbersCanBeAdded() {
-        // expect
-        assertEquals(valueOf(5), calculator.sum(valueOf(3), valueOf(2)));
+
+    private static Stream<Arguments> dataForCorrectSumCalculation() {
+        return Stream.of(
+                Arguments.of("two positive numbers can be added", 3, 2, 5),
+                Arguments.of("negative and positive numbers can be added", -3, 2, -1),
+                Arguments.of("two negative numbers can be added", -3, -2, -5)
+        );
     }
 
-    @Test
-    void negativeAndPositiveNumbersCanBeAdded() {
-        // expect
-        assertEquals(valueOf(-1), calculator.sum(valueOf(-3), valueOf(2)));
+    private static Stream<Arguments> dataForException() {
+        return Stream.of(
+                Arguments.of("null as first number is unsupported", null, ONE),
+                Arguments.of("null as second number is unsupported", ONE, null),
+                Arguments.of("null as both numbers is unsupported", null, null)
+        );
     }
 
-    @Test
-    void twoNegativeNumbersCanBeAdded() {
-        // expect
-        assertEquals(valueOf(-5), calculator.sum(valueOf(-3), valueOf(-2)));
+    @org.junit.jupiter.params.ParameterizedTest(name = "{index}. {0}")
+    @MethodSource("dataForCorrectSumCalculation")
+    void shouldCalculateSumCorrectly(String name, int num1, int num2, int expectedResult) {
+        assertEquals(valueOf(expectedResult), calculator.sum(valueOf(num1), valueOf(num2)));
     }
 
-    @Test
-    void nullAsFirstNumberIsUnsupported() {
-        // expect
+    @org.junit.jupiter.params.ParameterizedTest(name = "{index}. {0}")
+    @MethodSource("dataForException")
+    void shouldThrowException(String name, BigDecimal num1, BigDecimal num2) {
         assertThrows(
                 IllegalArgumentException.class,
-                () -> calculator.sum(null, ONE));
-    }
-
-    @Test
-    void nullAsSecondNumberIsUnsupported() {
-        // expect
-        assertThrows(
-                IllegalArgumentException.class,
-                () -> calculator.sum(ONE, null));
-    }
-
-    @Test
-    void nullAsBothNumbersIsUnsupported() {
-        // expect
-        assertThrows(
-                IllegalArgumentException.class,
-                () -> calculator.sum(null, null));
+                () -> calculator.sum(num1, num2));
     }
 }
