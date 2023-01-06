f0bfab7e08d1abfcc20651af3f964e582e983e78
MPawezka <marcin.pawezka@gmail.com>
2023-01-06 12:23:44 +0100

    day 4 solution

--- day004/src/main/java/com/pawelpluta/day004/WrappedString.java
+++ day004/src/main/java/com/pawelpluta/day004/WrappedString.java
@@ -2,6 +2,14 @@ package com.pawelpluta.day004;
 
 record WrappedString(String value) {
     boolean isPalindrome() {
-        return false;
+        String caseInsensitiveNoWhiteSpaceValue = value.toLowerCase().replaceAll("\s", "");
+
+        for (int i = 0; i < caseInsensitiveNoWhiteSpaceValue.length(); i++) {
+            if (caseInsensitiveNoWhiteSpaceValue.charAt(i) != caseInsensitiveNoWhiteSpaceValue.charAt(caseInsensitiveNoWhiteSpaceValue.length() - 1 - i)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
