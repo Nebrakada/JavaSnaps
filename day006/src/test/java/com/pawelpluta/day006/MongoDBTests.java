d74eedc6fa6db03c14b4ff81c0017325a46fe4b6
MPawezka <marcin.pawezka@gmail.com>
2023-01-08 20:57:40 +0100

    day 6 solution

--- day006/src/test/java/com/pawelpluta/day006/MongoDBTests.java
+++ day006/src/test/java/com/pawelpluta/day006/MongoDBTests.java
@@ -1,8 +1,15 @@
 package com.pawelpluta.day006;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import java.util.List;
 
@@ -11,11 +18,29 @@ import static com.pawelpluta.day006.PersonFixture.randomLastName;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Testcontainers
 class MongoDBTests {
 
     @Autowired
     PersonRepository personRepository;
 
+    @Container
+    static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
+
+    @BeforeEach
+    void setUp() {
+        mongoDBContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void mongoProps(DynamicPropertyRegistry registry) {
+        mongoDBContainer.start();
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+    void cleanup() {
+        mongoDBContainer.stop();
+
+    }
     @Test
     void personCouldBeFoundByItsLastName() {
         // given - person to find
