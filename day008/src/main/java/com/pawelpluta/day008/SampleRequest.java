a8113252d07c20324fec440515a893d162ded082
MPawezka <marcin.pawezka@gmail.com>
2023-01-11 16:36:06 +0100

    day 8 solution

--- day008/src/main/java/com/pawelpluta/day008/SampleRequest.java
+++ day008/src/main/java/com/pawelpluta/day008/SampleRequest.java
@@ -1,13 +1,16 @@
 package com.pawelpluta.day008;
 
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 
 record SampleRequest(
-        Integer integer,
-        BigDecimal decimal,
-        String text,
-        NestedRequestData data) {
+        @PositiveOrZero @NotNull Integer integer,
+        @NotNull @Negative BigDecimal decimal,
+        @NotNull @Size(min = 8, max = 16) String text,
+        @Valid NestedRequestData data) {
 
+    @MagicLogicValidation
     record NestedRequestData(
             Boolean magicLogicFlag,
             String magicLogicField) {
