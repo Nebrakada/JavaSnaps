46215965fbb37d8def43d914d38fe147266a3971
MPawezka <marcin.pawezka@gmail.com>
2023-01-13 19:40:48 +0100

    day 9 solution

--- day009/src/main/java/com/pawelpluta/day009/SwitchTypeMatching.java
+++ day009/src/main/java/com/pawelpluta/day009/SwitchTypeMatching.java
@@ -2,6 +2,15 @@ package com.pawelpluta.day009;
 
 class SwitchTypeMatching {
     MatchResults match(Object value) {
-        return null;
+
+        return switch (value) {
+            case Integer i -> MatchResults.forInteger();
+            case String s -> MatchResults.forString();
+            case Long l -> MatchResults.forLong();
+            case CharSequence c -> MatchResults.forCharSequence();
+            case Character cr -> MatchResults.forChar();
+            case null -> MatchResults.forNull();
+            default -> null;
+        };
     }
 }
